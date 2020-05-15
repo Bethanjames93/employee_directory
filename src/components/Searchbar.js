4ec55e9aed087cb4647685d19d9b62700f665023
Bethan James <bethanjames@Bethans-MacBook-Pro.local>
2020-05-15 22:26:31 +0100

    added handleChange to Search.js

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,9 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Search extends Component {
+class Search extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {employee: ''};
+    }
+
+    handleChange(event) {
+        this.setState({employee: event.target.value});
+    }
+
     render() {
         return (
-            <input className="form-control form-control-lg" type="text" placeholder="Enter Employee Name"></input>
+            <input 
+                className="form-control form-control-lg" 
+                type="text"    
+                placeholder="Enter Employee Name" 
+                value={this.state.employee} 
+                onChange={this.handleChange}>
+            </input>
         )
     }
 }
