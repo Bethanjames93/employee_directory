a5205055cd95e3cd68b3dcc7bb15f7744f9f3642
Bethan James <bethanjames@Bethans-MacBook-Pro.local>
2020-05-15 17:35:42 +0100

    added search box to enter employee name

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Header from './components/Header.js';
+import Search from './components/Searchbar.js';
 
 import './App.css';
 
 function App() {
   return (
-    <Header />
+    <div>
+      <Header />
+      <Search /> 
+    </div>
+
   );
 }
 
---
+++ src/components/Searchbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+    render() {
+        return (
+            <input className="form-control form-control-lg" type="text" placeholder="Enter Employee Name"></input>
+        )
+    }
+}
+
+export default Search;
